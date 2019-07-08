e4c0665c62bb5ca8e7990b3eba00b21fdc484e45
huipan <huipan.hnu@qq.com>
2019-07-08 17:47:50 +0800

    Update __init__.py
    surpport compile all java file of a directory.

--- jpb/__init__.py
+++ jpb/__init__.py
@@ -24,6 +24,18 @@ def main():
         if args.execute:
             print('----------java project builder (execute mode)----------')
             jpblib.execute_main_class(project_root, classpath)
+    elif os.path.isdir(args.mainfile):
+        if args.execute:
+            print('WARNING: directory as the args only support compile mode !')
+        if args.compile:
+            print('----------java project builder (compile mode)----------')
+            for rt, dirs, files in os.walk(args.mainfile):
+                for file in files:
+                    if os.path.splitext(file)[1]==".java":
+                        project_root = jpblib.find_project_root(args.profile, os.path.join(rt,file), args.proroot)
+                        classpath = jpblib.get_classpath(project_root, os.path.join(rt,file))
+                        print('-> %s' % classpath)
+                        jpblib.compile_class_file(project_root, classpath)
     else:
         print('File "%s" not found !' % args.mainfile)
 
